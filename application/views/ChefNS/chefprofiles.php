0162b8ebf60faeb87f281a0249165eff4a68025c
David Ogaro <38218784+DOgaro@users.noreply.github.com>
2018-10-02 10:49:29 +0300

    chef System

---
+++ application/views/ChefNS/chefprofiles.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Motor Trend</title>
+  <link rel="icon" type="image/ico" href="logo1.png">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link rel="stylesheet" href="<?php echo base_url('assets/https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');?>">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/style.css');?>">
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
+
+
+<div id="band" class="container text-center">
+  <h3>CHEFS</h3>
+  <p><em>We love vehicles!</em></p>
+  <p>We have created an exquisite motorshow website just for you. Brace yourself for some of the fiery rides on the face of the planet. YES!! You heard it all right, it's the hot wheels we are vouging for. Join us for we never disappoint our loyal car fans. Receive first hand exclusive deals from the top motorshow website in the market. Trust us cause we trust you. You found us, now we'll find you with our car packs. Feel free to contact us anytime with your queries and suggestions. #BE_DOPE #LOVECARS #REALERS</p>
+  <br>
+  <div class="row">
+    <div class="col-sm-4">
+      <p class="text-center"><strong>Rodney Bakari</strong></p><br>
+      <a href="#demo" data-toggle="collapse">
+        <img src="assets/img/avatar7.png" class="img-circle person" alt="Random Name" width="255" height="255">
+      </a>
+      <div id="demo" class="collapse">
+        <p>Name: Rodney Bakari</p>
+        <p>Email: rodneybakari@gmail.com</p>
+        <p>Company: Shade</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <p class="text-center"><strong>David Ogaro</strong></p><br>
+      <a href="#demo2" data-toggle="collapse">
+        <img src="assets/img/avatar7.png" class="img-circle person" alt="Random Name" width="255" height="255">
+      </a>
+      <div id="demo2" class="collapse">
+        <p>Name: David Ogaro</p>
+        <p>Email: davyr4@gmail.com</p>
+        <p>Company: Charlie's Bistro</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <p class="text-center"><strong>Jamez Boro</strong></p><br>
+      <a href="#demo3" data-toggle="collapse">
+        <img src="assets/img/avatar7.png" class="img-circle person" alt="Random Name" width="255" height="255">
+      </a>
+      <div id="demo3" class="collapse">
+        <p>Name: Jamez Boro</p>
+        <p>Email: jamezboro@gmail.com</p>
+        <p>Company: Siwaka</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <p class="text-center"><strong>Stephen Kimani</strong></p><br>
+      <a href="#demo4" data-toggle="collapse">
+        <img src="assets/img/avatar7.png" class="img-circle person" alt="Random Name" width="255" height="255">
+      </a>
+      <div id="demo4" class="collapse">
+        <p>Name: Stephen Kimani</p>
+        <p>Email: kymtopaz@gmail.com</p>
+        <p>Company: KFC</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <p class="text-center"><strong>Meghan Mueni</strong></p><br>
+      <a href="#demo5" data-toggle="collapse">
+        <img src="assets/img/avatar7.png" class="img-circle person" alt="Random Name" width="255" height="255">
+      </a>
+      <div id="demo5" class="collapse">
+        <p>Name: Meghan Mueni</p>
+        <p>Email: meghanmueni@gmail.com</p>
+        <p>Company: The Hub</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <p class="text-center"><strong>Wanjiru Kinuthia</strong></p><br>
+      <a href="#demo6" data-toggle="collapse">
+        <img src="assets/img/avatar7.png" class="img-circle person" alt="Random Name" width="255" height="255">
+      </a>
+      <div id="demo6" class="collapse">
+        <p>Name: Wanjiru Kinuthia</p>
+        <p>Email: wajirukinuthia@gmail.com</p>
+        <p>Company: Kempinski</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Footer -->
+<footer class="text-center">
+  <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a><br><br>
+  <p>© 2018 CHEF HUB | CHEF NETWORKING GROUP, LLC. ALL RIGHTS RESERVED.</p> 
+</footer>
+</body>
+</html>
